292c2ffdd54042cd9930ff7a4769504e479ee961
Arnav Sugavanam <asugavan@unc.edu>
2023-12-04 19:33:19 -0500

    final version

--- roster.py
+++ roster.py
@@ -2,9 +2,13 @@
 
 import pandas as pd
 
-player = {"Last Name": ["Bacot", "Davis", "Ryan", "Cadeau"],
-          "First Name": ["Armando", "RJ", "Cormac", "Elliot"],
-          "height": [83,72,195,73],
-          "wieght":[240,180,195,180]}
+player = {"Last Name": ["Bacot", "Davis", "Ryan", "Cadeau", "High", "Trimble","Wojcik","Washington", "Lebo", "Landry", "Ingram" ],
+          "First Name": ["Armando", "RJ", "Cormac", "Elliot", "Zayden", "Seth","Paxon","Jalen", "Creighton", "Rob", "Harrison"],
+          "height": [83,72,76,72,81,75,77,82,73,76,79],
+          "weight":[240,180,195,180,225,195,195,230,180,190,235]}
 data = pd.DataFrame(player)
+
+data["bmi"] = round((data["weight"]/2.205)/((data["height"]/39.37)**2), 2)
 print(data)
+
+data.to_csv("bmi.csv")
